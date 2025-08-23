c99f41ef9ae0d2c75747aaba23e2661e4e481902
Gongkirby <9040__@naver.com>
2025-08-23 19:41:26 +0900

    feat : 로그인, 회원가입 DB 연결

--- fe/src/components/Header.js
+++ fe/src/components/Header.js
@@ -20,7 +20,7 @@ function Header() {
 
             <div className={styles['header-buttons']}>
                 <button onClick={() => navigate("/login")}>로그인</button>
-                <button onClick={() => navigate("/register")}>회원가입</button>
+                <button onClick={() => navigate("/signup")}>회원가입</button>
             </div>
         </header>
     );
